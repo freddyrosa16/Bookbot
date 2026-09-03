6bc187cc5673ab9c5459e9136e07dc70a31ee2b7
Freddy Rosa <321212545+freddyrosa16@users.noreply.github.com>
2026-09-03 15:09:55 -0400

    added 2 more books and refractored the hard coded path

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import sys
 from stats import word_count, char_count, chars_dict_to_sorted_list
 
 
 def main():
-    print_report("books/frankenstein.txt", word_count, chars_dict_to_sorted_list)
+    print('Usage: python3 main.py <path_to_book>')
+    path = sys.argv[1]
+    print_report(path, word_count, chars_dict_to_sorted_list)
 
 def print_report(file_path, word_count, sorted_lst):
     count = word_count(file_path)
