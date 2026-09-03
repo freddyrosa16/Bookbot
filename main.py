e3a0527ed53523f616647951af4140205bea028b
Freddy Rosa <321212545+freddyrosa16@users.noreply.github.com>
2026-09-03 14:58:22 -0400

    created print_report for the book it prints the book, total words in the book and chracter counts

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from stats import word_count, char_count, chars_dict_to_sorted_list
 
 
 def main():
-    print(f'Found {word_count("books/frankenstein.txt")} total words')
-    print(f'{chars_dict_to_sorted_list("books/frankenstein.txt")}')
+    print_report("books/frankenstein.txt", word_count, chars_dict_to_sorted_list)
+
+def print_report(file_path, word_count, sorted_lst):
+    count = word_count(file_path)
+    sorted_lst = chars_dict_to_sorted_list(file_path)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {file_path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {count} total words')
+    print('--------- Character Count -------')
+    for key in sorted_lst:
+        if key[0].isalpha():
+            print(f'{key[0]}: {key[1]}')
+    print('============= END ===============')
 
 main()
